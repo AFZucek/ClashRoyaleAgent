c652a55c9185729730cb15ffa8683abde54f12ce
MatthieuFoucu <mfoucu@gmail.com>
2025-04-23 17:31:02 -0400

    Update info_extractor.py

--- info_extractor.py
+++ info_extractor.py
@@ -1,1 +1,16 @@
-# extract all the necessary information on a given game state
+# extract all the necessary information on a given game state (image)
+
+#### IMPORTS ####
+
+def extract():
+    """Puts together information extracted by OCR in the game"""
+
+    # time left
+    
+    # Enemy tower information
+
+    # Cards on screen, oponent/user
+
+    # User tower information
+
+    # Cards in hand w/their information
