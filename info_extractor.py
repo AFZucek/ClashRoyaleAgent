cbcdd5ed8c92b006b44bfe683f8cfa80f65e833f
MatthieuFoucu <mfoucu@gmail.com>
2025-04-23 17:43:10 -0400

    Added get_time

--- info_extractor.py
+++ info_extractor.py
@@ -1,11 +1,15 @@
 # extract all the necessary information on a given game state (image)
 
 #### IMPORTS ####
+from PIL import Image
+import pytesseract
 
-def extract():
+
+def extract(image):
     """Puts together information extracted by OCR in the game"""
 
-    # time left
+    # time left in seconds
+    time_remaining = get_time(image)
     
     # Enemy tower information
 
@@ -14,3 +18,14 @@ def extract():
     # User tower information
 
     # Cards in hand w/their information
+
+def get_time(image):
+    """get Image from top right, uses OCR for the time in seconds"""
+
+    # Extract Top Right
+
+    # Run OCR
+    time_string = pytesseract.image_to_string(image)
+    minutes, seconds = map(int, time_string.split(":"))
+
+    return minutes * 60 + seconds
