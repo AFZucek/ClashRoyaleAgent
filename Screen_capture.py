e0243ef6518138ced4ee48227fdc4c4d8d760940
AFZucek <azucek@purdue.edu>
2025-04-23 19:10:34 -0400

    Emulation Screen Capture Test
    Adding first test for emulation screen capture. Brings BlueStacks Air emulator to the front, then captures its bounding box. Todo: crop headers out.

--- Screen_capture.py
+++ Screen_capture.py
@@ -1,5 +1,52 @@
-# takes a screen capture of the emulator to get fed into the info_extraction.py
+from PIL import ImageGrab
+from Quartz import CGWindowListCopyWindowInfo, kCGWindowListOptionOnScreenOnly, kCGNullWindowID
+import os
+import subprocess
 
-#### IMPORTS ####
 
-print("Hi")
+def focus_bluestacks():
+    """
+    Bring blue stacks emulator to the front
+    """
+    subprocess.run([
+        "osascript", "-e", 'tell application "BlueStacks" to activate'
+    ])
+
+def find_bluestacks_window():
+    """
+    Find bounding box for bluestacks_window
+    """
+    window_list = CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly, kCGNullWindowID)
+    for window in window_list:
+        owner_name = window.get('kCGWindowOwnerName', '')
+        window_name = window.get('kCGWindowName', '')
+        if 'BlueStacks' in owner_name or 'BlueStacks' in window_name:
+            bounds = window['kCGWindowBounds']
+            x = int(bounds['X'])
+            y = int(bounds['Y'])
+            w = int(bounds['Width'])
+            h = int(bounds['Height'])
+            return (x, y, x + w, y + h)
+    return None
+
+
+def main():
+    """
+    Main function, currently testing capture for one image
+    """
+
+    focus_bluestacks()
+
+    bbox = find_bluestacks_window()
+
+    test_file_path = os.path.join('ClashRoyaleAgent', 'data', 'TestCaptures', 'testscreen.png')
+
+    if not bbox:
+        raise Exception("BlueStacks window not found.")
+
+    # Capture and savecle
+    img = ImageGrab.grab(bbox)
+    img.save(test_file_path)
+
+if __name__ == "__main__":
+    main()
