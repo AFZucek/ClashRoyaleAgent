ce084b25dfa7562244828d6bfb837e09857a27f0
Matthieu Foucu <mfoucu@gmail.com>
2025-05-14 20:45:16 -0700

    Program to train CNN classifier
    Set up the basics of this program with the imports and the global parameters.

---
+++ card_classifier.py
@@ -0,0 +1,18 @@
+###imports###
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import zipfile
+
+
+# Parameters
+DATA_DIR = "C:\Users\mfouc\OneDrive\Desktop\Clash_Cards_Data" # replace with your path
+BATCH_SIZE = 32 # depends on cpu/gpu
+NUM_EPOCHS = 10 # passes over data
+IMG_SIZE = 128
+MODEL_PATH = "Models/classifier.pth"
+
+# train model with torch
