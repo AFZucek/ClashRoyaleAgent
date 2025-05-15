212849f89e4ba5ac9fc718f8b08965f48a3988e3
AFZucek <azucek1@gmail.com>
2025-05-15 09:26:03 -0700

    Automated running training battle
    Added function to automatically start training battle, also updated a little bit of logic to slightly optimize.

--- Screen_capture.py
+++ Screen_capture.py
@@ -5,27 +5,18 @@ import pyautogui
 import time
 import os
 
+#moved path creation outside of function
+path = os.path.join("data", "TestCaptures", "testscreen.png")
+os.makedirs(os.path.dirname(path), exist_ok=True)
 
-def capture_bluestacks():
+def capture_bluestacks(win):
     """take screen shot of blue stacks window"""
-
-    window = gw.getWindowsWithTitle("BlueStacks")
-
-    if not window:
-        raise Exception("BlueStacks window not found")
-    win = window[0]
-
-    # pop it to the front
-    win.activate()
-    time.sleep(0.03)  # give it a moment to come to front
 
     x, y, w, h = win.left, win.top, win.width, win.height
 
     img = pyautogui.screenshot(region=(x, y, w, h))
 
     # save image
-    path = os.path.join("data", "TestCaptures", "testscreen.png")
-    os.makedirs(os.path.dirname(path), exist_ok=True)
     img.save(path)
     return img
 
