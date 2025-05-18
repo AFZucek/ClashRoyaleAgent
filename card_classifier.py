d28cea3d102d9e6bc44b4a5d6353fdcc0812178e
AFZucek <azucek1@gmail.com>
2025-05-18 15:05:08 -0700

    100% done with CNN
    First try got CNN to work (jk nothing worked)

--- card_classifier.py
+++ card_classifier.py
@@ -11,6 +11,10 @@ from Screen_capture import pyautogui
 import pygetwindow as gw
 import time
 
+
+#grab pre made model
+from ultralytics import YOLO
+
 """
 # Parameters
 DATA_DIR = "C:/Users/mfouc/OneDrive/Desktop/Clash_Cards_Data/" # error here ??
@@ -65,4 +69,23 @@ def collect_images(interval):
         time.sleep(interval)
 
 
-collect_images(2)
+def train():
+
+    # Load a pretrained YOLO model (recommended for training)
+    model = YOLO("yolo11n.pt")
+
+    # Train the model using the 'coco8.yaml' dataset for 3 epochs
+    results = model.train(data="D:/ClashData/CNNYOLO8Data/data.yaml", epochs=3)
+
+    # Evaluate the model's performance on the validation set
+    results = model.val()
+
+    # Perform object detection on an image using the model
+    results = model("D:/ClashData/CNN_Test_Images/image_1747453019.png", save = True)
+
+    # Export the model to ONNX format
+    success = model.export(format="onnx")
+
+#collect_images(2)
+
+train()
